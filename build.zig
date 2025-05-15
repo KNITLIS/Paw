065601fd9442f22ca84adabbdcd9cde04e2bb02d
Knitlis <113023091@mail.ru>
2025-05-15 07:00:20 +0600

    style: shuffle build options to more suiting places

--- build.zig
+++ build.zig
@@ -7,9 +7,20 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .target = kernel_target,
         .optimize = .Debug,
+        // Disable features that are problematic in kernel space.
+        .red_zone = false,
+        .stack_check = false,
+        .stack_protector = false,
     });
 
     const kernel = b.addExecutable(.{ .name = "kernel", .root_module = exe_mod });
+    kernel.pie = false;
+    kernel.want_lto = false;
+    // Delete unused sections to reduce the kernel size.
+    kernel.link_function_sections = true;
+    kernel.link_data_sections = true;
+    kernel.link_gc_sections = true;
+    kernel.link_z_max_page_size = 0x1000;
     kernel.addAssemblyFile(b.path("src/bootloader.s"));
     kernel.setLinkerScript(b.path("src/link_kernel.ld"));
     b.installArtifact(kernel);
