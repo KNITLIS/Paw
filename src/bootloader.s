264f6b86fefca706843144997a11f253be36ccb8
Knitlis <113023091@mail.ru>
2025-05-15 07:11:09 +0600

    fix: modify linker script to load entire kernel on start

--- src/bootloader.s
+++ src/bootloader.s
@@ -1,54 +1,12 @@
 .section .text.bootentry # section name referenced by linker script (src/link_kernel.ld)
 .code16
 
-jmp _start
-
-bootDisk: 
-    .byte 0
-sectorsToRead:
-    .byte 100       #TODO: should be extended with growth of kernel
-readErrorMessage: 
-    .asciz "Disk read error"
-
-# Segment descriptor https://wiki.osdev.org/Global_Descriptor_Table
-# info ||     Limit     |         Base         |            Access byte            |     Limit     |         Flags         |     Base      ||
-# size ||     0-15      |         0-23         | P |   DPL   | S | E | DC | RW | A |     16-19     | G | DB | L | Reserved |     24-31     ||
-#                                                                                  |  Written as a single byte with Limit  |
-#                                                                                  |       being a lower part of it        |
-
-GDT_Start:
-    Null_Descriptor:
-        .long 0
-        .long 0
-    Code_descriptor:
-        .word 0xFFFF      # limit
-        .word 0           # base
-        .byte 0
-        .byte 0b10011010  # access byte
-        .byte 0b11001111  # limit + flags
-        .byte 0           # base
-    Data_Descriptor:
-        .word 0xFFFF      # limit
-        .word 0           # base
-        .byte 0
-        .byte 0b10010010  # access byte
-        .byte 0b11001111  # limit + flags
-        .byte 0           # base
-GDT_End:
-
-GDT_Descriptor:
-    .word GDT_End - GDT_Start - 1
-    .long GDT_Start
-
-CODE_SEG = Code_descriptor - GDT_Start
-DATA_SEG = Data_Descriptor - GDT_Start
-
 _start:
     movb %dl, bootDisk
 
     read_kernel:    #load kernel from disk
         movb $2, %ah
-        movb sectorsToRead, %al
+        movb sectors_to_load, %al
         movb $0, %ch
         movb $2, %cl
         movb $0, %dh
@@ -88,6 +46,8 @@ _start:
 
     ljmp $CODE_SEG, $start_protected_mode
 
+
+
 .text
 .code32
 
@@ -95,3 +55,48 @@ _start:
 
 start_protected_mode:
     jmp _kernel_entry
+
+
+
+.data
+
+bootDisk: 
+    .byte 0
+readErrorMessage: 
+    .asciz "Disk read error"
+
+# Segment descriptor https://wiki.osdev.org/Global_Descriptor_Table
+# info ||     Limit     |         Base         |            Access byte            |     Limit     |         Flags         |     Base      ||
+# size ||     0-15      |         0-23         | P |   DPL   | S | E | DC | RW | A |     16-19     | G | DB | L | Reserved |     24-31     ||
+#                                                                                  |  Written as a single byte with Limit  |
+#                                                                                  |       being a lower part of it        |
+
+GDT_Start:
+    Null_Descriptor:
+        .long 0
+        .long 0
+    Code_descriptor:
+        .word 0xFFFF      # limit
+        .word 0           # base
+        .byte 0
+        .byte 0b10011010  # access byte
+        .byte 0b11001111  # limit + flags
+        .byte 0           # base
+    Data_Descriptor:
+        .word 0xFFFF      # limit
+        .word 0           # base
+        .byte 0
+        .byte 0b10010010  # access byte
+        .byte 0b11001111  # limit + flags
+        .byte 0           # base
+GDT_End:
+
+GDT_Descriptor:
+    .word GDT_End - GDT_Start - 1
+    .long GDT_Start
+
+CODE_SEG = Code_descriptor - GDT_Start
+DATA_SEG = Data_Descriptor - GDT_Start
+
+sectors_to_load:    #last byte of data segment is filled by linker 
+                    #TODO: can overflow byte if kernel grow too big, fix somehow
